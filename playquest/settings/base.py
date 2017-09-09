7223ccac92a466de2f50378c62cc803d5f787cfd
mrbrian88 <briancaffey2010@gmail.com>
2017-09-09 10:17:36 -0400

    changed database settings

--- playquest/settings/base.py
+++ playquest/settings/base.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = 'playquest.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'deb4m9n9tv2saj',
+    'USER': 'mydatabaseuser',
+    'PASSWORD': 'xuavukifzjrqvx',
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
 }
-
 import dj_database_url
 
 db_from_env = dj_database_url.config()
