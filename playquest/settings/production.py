70e308e6027aeecd637eacc84dfbd427e4d843d9
mrbrian88 <briancaffey2010@gmail.com>
2017-09-09 16:58:15 -0400

    added crispy forms

--- playquest/settings/production.py
+++ playquest/settings/production.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'plqst',
     'rest_framework',
     'accounts',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- playquest/urls.py
+++ playquest/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+
 
     #playquest
     url(r'^', include('plqst.urls', namespace='plqst')),
