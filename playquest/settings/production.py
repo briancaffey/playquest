1055e0fef9a61c6007af8e73ac8e8f701bc69448
mrbrian88 <briancaffey2010@gmail.com>
2017-09-08 22:46:55 -0400

    tried to fix db

--- playquest/settings/production.py
+++ playquest/settings/production.py
@@ -77,16 +77,14 @@ WSGI_APPLICATION = 'playquest.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     # 'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 import dj_database_url
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
