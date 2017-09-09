a6d1ce52ec86203f7bc3367b7ca228807b31f417
mrbrian88 <briancaffey2010@gmail.com>
2017-09-08 21:23:39 -0400

    added JSON parser from DRF

--- plqst/api/views.py
+++ plqst/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView,
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 # from rest_framework import authentication, permissions, serializers
 
 
